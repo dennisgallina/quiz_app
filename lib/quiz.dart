c9ef91e010eeaa6fa52acdc8748e07e4c4c0493d
Dennis Gallina <dennis.gallina@gmail.com>
2023-12-23 14:39:40 +0100

    Update.

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -1,42 +1,51 @@
 import 'package:flutter/material.dart';
-import 'package:quiz/data/questions.dart';
-import 'package:quiz/questions_screen.dart';
-import 'package:quiz/results_screen.dart';
-import 'package:quiz/start_screen.dart';
+import 'package:quiz/data/questions.dart'; // Imports the questions data file
+import 'package:quiz/questions_screen.dart'; // Imports the QuestionsScreen widget
+import 'package:quiz/results_screen.dart'; // Imports the ResultsScreen widget
+import 'package:quiz/start_screen.dart'; // Imports the StartScreen widget
 
 class Quiz extends StatefulWidget {
+  // Constructor for the Quiz widget
   const Quiz({super.key});
 
   @override
-  State<Quiz> createState() {
-    return _QuizState();
-  }
+  // Creates the state for the Quiz widget
+  State<Quiz> createState() => _QuizState();
 }
 
 class _QuizState extends State<Quiz> {
+  // List to store the selected answers
   List<String> selectedAnswers = [];
-  // The question mark allow to assign a null value
+
+  // Widget variable to hold the currently active screen
+  // The question mark indicates that the value can be null
   Widget? activeScreen;
 
-  // Executed when the StatefulWidget's State object is initialized
+  // Function called when the widget is first initialized
   @override
   void initState() {
-    // If pass a function in an Object Constructor, it can be used whereever
+    // Set the initial active screen to the StartScreen
     activeScreen = StartScreen(switchScreen);
+
     super.initState();
   }
 
+  // Function to switch to the QuestionsScreen
   void switchScreen() {
-    // Re-execute the built() an other
+    // Re-build the widget to reflect the change
     setState(() {
       activeScreen = QuestionsScreen(onSelectAnswer: chooseAnswer);
     });
   }
 
+  // Function to handle the user's selected answer
   void chooseAnswer(String answer) {
+    // Add the selected answer to the list
     selectedAnswers.add(answer);
 
+    // Check if all questions have been answered
     if (selectedAnswers.length == questions.length) {
+      // Navigate to the ResultsScreen
       setState(() {
         activeScreen = ResultsScreen(
           chosenAnswer: selectedAnswers,
@@ -46,18 +55,24 @@ class _QuizState extends State<Quiz> {
     }
   }
 
+  // Function to restart the quiz
   void restartQuiz() {
+    // Reset the selected answers list
     selectedAnswers = [];
+
+    // Navigate back to the StartScreen
     setState(() {
       activeScreen = StartScreen(switchScreen);
     });
   }
 
-  // Executed when the Widget is built for the first time
+  // Function to build the widget for the first time
   @override
   Widget build(context) {
+    // Create a MaterialApp widget
     return MaterialApp(
       home: Scaffold(
+        // Set the background color to a gradient
         body: Container(
           decoration: const BoxDecoration(
             gradient: LinearGradient(
@@ -69,6 +84,7 @@ class _QuizState extends State<Quiz> {
               end: Alignment.bottomRight,
             ),
           ),
+          // Add the active screen to the body of the Scaffold
           child: activeScreen,
         ),
       ),
--- lib/results_screen.dart
+++ lib/results_screen.dart
@@ -4,15 +4,21 @@ import 'package:quiz/data/questions.dart';
 import 'package:quiz/questions_summary/questions_summary.dart';
 
 class ResultsScreen extends StatelessWidget {
+  // This widget displays the results of the quiz
+
   const ResultsScreen({
     super.key,
-    required this.chosenAnswer,
-    required this.onTap,
+    required this.chosenAnswer, // List of selected answers by the user
+    required this.onTap, // Callback to restart the quiz
   });
 
+  // List of selected answers by the user
   final List<String> chosenAnswer;
+
+  // Callback to restart the quiz
   final void Function() onTap;
 
+  // Function to generate the summary data
   List<Map<String, Object>> getSummaryData() {
     final List<Map<String, Object>> summary = [];
 
@@ -27,49 +33,72 @@ class ResultsScreen extends StatelessWidget {
     return summary;
   }
 
+  // Build the widget for the ResultsScreen
   @override
   Widget build(BuildContext context) {
+    // Retrieve the summary data from the getSummaryData() function
     final summaryData = getSummaryData();
+
+    // Calculate the number of total and correct questions
     final int numberTotalQuestions = questions.length;
-    final numberCorrectQuestions = summaryData.where((data) {
+    final int numberCorrectQuestions = summaryData.where((data) {
       return data['user_answer'] == data['correct_answer'];
     }).length;
 
+    // Create a SizedBox widget with the specified width and height
     return SizedBox(
-      // Sets the width to double.infinity
-      width: double.infinity,
-      // Wraps the question and answer buttons in a Container
+      width: double.infinity, // Set the width to double.infinity
       child: Container(
-        // Adds a margin of 40 pixels around the widget
+        // Add a margin of 40 pixels around the widget
         margin: const EdgeInsets.all(40),
+
+        // Display the quiz results
         child: Column(
-          // Aligns the children along the main axis (vertically)
+          // Align children along the main axis (vertically)
           mainAxisAlignment: MainAxisAlignment.center,
-          // Aligns the children within their respective containers (stretch)
+
+          // Align children within their respective containers (stretch)
           crossAxisAlignment: CrossAxisAlignment.stretch,
+
+          // Display the summary text
           children: [
+            // Display the user's score
             Text(
-              'You answered $numberCorrectQuestions out of $numberTotalQuestions questions correctly!',
+              'You answered ${numberCorrectQuestions} out of ${numberTotalQuestions} questions correctly!',
               style: GoogleFonts.lato(
-                color: Colors.white,
-                fontSize: 20,
+                color: Colors.white, // Set the text color to white
+                fontSize: 20, // Set the text font size to 20 pixels
               ),
-              textAlign: TextAlign.center,
+              textAlign: TextAlign.center, // Align the text to the center
             ),
-            const SizedBox(height: 30),
-            QuestionsSummary(summaryData: summaryData),
-            const SizedBox(height: 30),
+
+            const SizedBox(
+                height: 30), // Add spacing between the title and the summary
+
+            // Display the questions summary
+            QuestionsSummary(
+              // Pass the summary data to the QuestionsSummary widget
+              summaryData: summaryData,
+            ),
+
+            const SizedBox(
+                height:
+                    30), // Add spacing between the summary and the 'Restart Quiz' button
+
+            // Create a TextButton.icon widget to display the 'Restart Quiz' button
             TextButton.icon(
+              // Call the onTap callback to restart the quiz
               onPressed: onTap,
-              icon: const Icon(
-                Icons.refresh_outlined,
-                color: Colors.white,
-              ),
+
+              // Set the icon to a refresh icon
+              icon: const Icon(Icons.refresh_outlined),
+
+              // Set the button text to 'Restart Quiz!'
               label: const Text(
                 'Restart Quiz!',
                 style: TextStyle(color: Colors.white),
               ),
-            )
+            ),
           ],
         ),
       ),
